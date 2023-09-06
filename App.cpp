1b218aca005c1b094f08a07fc683cbb89848b997
Tom Tsagkatos <TomTsagkatos@afloof.dev>
2023-09-06 18:45:49 +0100

    Try sample triangle

--- App.cpp
+++ App.cpp
@@ -26,6 +26,45 @@ ComPtr<ID3D11DeviceContext1> devcon;
 ComPtr<IDXGISwapChain1> swapchain;
 ComPtr<ID3D11RenderTargetView> rendertarget;
 
+// a struct to represent a single vertex
+struct VERTEX
+{
+	float X, Y, Z;    // vertex position
+};
+
+ComPtr<ID3D11Buffer> vertexbuffer;    // defined in CGame
+
+// shaders
+ComPtr<ID3D11VertexShader> vertexshader;
+ComPtr<ID3D11PixelShader> pixelshader;
+ComPtr<ID3D11InputLayout> inputlayout;
+
+#include <fstream>
+
+// this function loads a file into an Array^
+Array<byte>^ LoadShaderFile(std::string File)
+{
+	Array<byte>^ FileData = nullptr;
+
+	// open the file
+	std::ifstream VertexFile(File, std::ios::in | std::ios::binary | std::ios::ate);
+
+	// if open was successful
+	if(VertexFile.is_open())
+	{
+		// find the length of the file
+		int Length = (int)VertexFile.tellg();
+
+		// collect the file data
+		FileData = ref new Array<byte>(Length);
+		VertexFile.seekg(0, std::ios::beg);
+		VertexFile.read(reinterpret_cast<char*>(FileData->Data), Length);
+		VertexFile.close();
+	}
+
+	return FileData;
+}
+
 // The main function is only used to initialize our IFrameworkView class.
 [Platform::MTAThread]
 int main(Platform::Array<Platform::String^>^)
@@ -177,6 +216,64 @@ void D3D11AvdlApplication::Run()
 	// create a render target pointing to the back buffer
 	dev->CreateRenderTargetView(backbuffer.Get(), nullptr, &rendertarget);
 
+	/*
+	 * View port
+	 */
+	// set the viewport
+	D3D11_VIEWPORT viewport = {0};
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = Window->Bounds.Width;
+	viewport.Height = Window->Bounds.Height;
+
+	devcon->RSSetViewports(1, &viewport);
+
+	/*
+	 * Triangle
+	 */
+	// create a triangle out of vertices
+	VERTEX OurVertices[] =
+	{
+		{ 0.0f, 0.5f, 0.0f },
+		{ 0.45f, -0.5f, 0.0f },
+		{ -0.45f, -0.5f, 0.0f },
+	};
+
+	// create the vertex buffer
+	D3D11_BUFFER_DESC bd = {0};
+	bd.ByteWidth = sizeof(VERTEX) * ARRAYSIZE(OurVertices);
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+	D3D11_SUBRESOURCE_DATA srd = {OurVertices, 0, 0};
+
+	dev->CreateBuffer(&bd, &srd, &vertexbuffer);
+
+	/*
+	 * shaders
+	 */
+	load the shader files
+	Array<byte>^ VSFile = LoadShaderFile("VertexShader.cso");
+	Array<byte>^ PSFile = LoadShaderFile("PixelShader.cso");
+
+	// create the shader objects
+	dev->CreateVertexShader(VSFile->Data, VSFile->Length, nullptr, &vertexshader);
+	dev->CreatePixelShader(PSFile->Data, PSFile->Length, nullptr, &pixelshader);
+
+	// set the shader objects as the active shaders
+	devcon->VSSetShader(vertexshader.Get(), nullptr, 0);
+	devcon->PSSetShader(pixelshader.Get(), nullptr, 0);
+
+	// initialize input layout
+	D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
+
+	// create and set the input layout
+	dev->CreateInputLayout(ied, ARRAYSIZE(ied), VSFile->Data, VSFile->Length, &inputlayout);
+	devcon->IASetInputLayout(inputlayout.Get());
+
 	while (!m_windowClosed)
 	{
 		if (m_windowVisible)
@@ -197,12 +294,26 @@ void D3D11AvdlApplication::Run()
 				m_deviceResources->Present();
 			}
 			*/
-			// set our new render target object as the active render target
-			devcon->OMSetRenderTargets(1, rendertarget.GetAddressOf(), nullptr);
-
 			// clear the back buffer to a deep blue
 			float color[4] = {0.0f, 0.2f, 0.4f, 1.0f};
 			devcon->ClearRenderTargetView(rendertarget.Get(), color);
+
+			// set our new render target object as the active render target
+			devcon->OMSetRenderTargets(1, rendertarget.GetAddressOf(), nullptr);
+
+			/*
+			 * Render triangle
+			 */
+			// set the vertex buffer
+			UINT stride = sizeof(VERTEX);
+			UINT offset = 0;
+			devcon->IASetVertexBuffers(0, 1, vertexbuffer.GetAddressOf(), &stride, &offset);
+
+			// set the primitive topology
+			devcon->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+			// draw 3 vertices, starting from vertex 0
+			devcon->Draw(3, 0);
 
 			// switch the back buffer and the front buffer
 			swapchain->Present(1, 0);
